ad52cee30a702794b20f01a1756d156175732198
johnycage <chemicalsequence@Yahoo.com>
2019-07-11 08:40:22 +0530

    removed silly sentence.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('user-agent.php');
 ?>
 </head>
 <body>
-You are a test subject. <br />
-Visit Again.
+If the page doesn't redirectly automatically, then please visit the Maps app directly.<br />
+Thank you. 
 </body>
 </html>
