113933a235b394510188b5b852b49d0542ef64d8
johnycage <chemicalsequence@Yahoo.com>
2019-07-11 08:41:52 +0530

    typos

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
 <head>
 <?php 
-include('user-agent.php');
+    include('user-agent.php');
 ?>
 </head>
 <body>
-If the page doesn't redirectly automatically, then please visit the Maps app directly.<br />
+If the page doesn't redirect automatically, then please visit the Maps app directly.<br />
 Thank you. 
 </body>
 </html>
